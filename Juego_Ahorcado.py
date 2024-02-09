27c8ea3084def7693eac332593399f363dd50bab
idoceb00 <idoceb00@estudiantes.unileon.es>
2024-02-09 13:46:07 +0100

    Falta añadir más diccionarios, para dar la opción de escoger la temática del juego, y corregir ciertos aspectos de la programación, como estandarizar el uso de lower()

--- Juego_Ahorcado.py
+++ Juego_Ahorcado.py
@@ -3,7 +3,7 @@ from random import choice
 
 def pide_letra():
     # Pedirle al usuario que escoja una letra
-    return input("¿Escoge una letra?: ")
+    return input("\n¿Escoge una letra?: ")
 
 
 def letra_is_valid(letra):
@@ -39,8 +39,8 @@ def is_win(secret_word, letras_correctas):
 
 def mostrar(secret_word, letras_correctas):
     word = ""
-    for letra in secret_word.lower():
-        if letra in letras_correctas:
+    for letra in secret_word:
+        if letra.lower() in letras_correctas:
             word += letra
         else:
             word += "-"
@@ -49,9 +49,17 @@ def mostrar(secret_word, letras_correctas):
 
 
 # FLUJO PRINCIPAL DE EJECUCIÓN
-dictionary = ("Ordenador", "Portátil", "Tablet", "Bolsa de Valores", "Cargador", "Elecciones", "Voto", "Mochila"
-              , "Persiana", "Camiseta", "Reloj")
-secret_word = choice(dictionary)
+dictionary_variado = (
+"Ordenador", "Tablet", "Bolsa de Valores", "Cargador", "Elecciones", "Voto", "Mochila", "Persiana",
+"Camiseta", "Reloj", "Vodka", "Calculadora", "Paraguas", "Pasamontañas", "Flexo", "Freixo",
+"Aguacate", "Silla", "Altavoz", "Abogado", "Profesor", "Conserje", "Director", "Silla de ruedas",
+"Masaje", "Comprensión lectora", "Amor propio", "Narcisismo", "Libreta", "Goma de borrar", "Zapatilla",
+"Cerveza", "Vino", "Galicia",)
+
+dictionary_galicia = ("A Coruña", "Sillobre", "Fene", "Ferrol", "Pontevedra", "Vigo", "Ourense", "Fisterra", "Cee",
+                      "Ribeira", "Cambados", "Vilagarcía de Arousa", "Camariñas")
+
+secret_word = choice(dictionary_galicia)
 letras_correctas = []
 letras_incorrectas = []
 vidas = 6
@@ -62,21 +70,26 @@ print(f"Bienvenido al juego del ahorcado!\nDebes adivinar la siguiente palabra s
       f"y tienes un máximo de {vidas} errores posibles\nTu palabra secreta es:\n")
 
 while fin == 1:
-    print(secret_word)
-    print(mostrar(secret_word, letras_correctas) + f"\nLetras incorrectas: {letras_incorrectas}\nVIDAS: {vidas}")
+    # print(secret_word)
+    print(mostrar(secret_word, letras_correctas) + f"\nVIDAS: {vidas}\nLetras incorrectas: {letras_incorrectas}")
 
     letra = pide_letra()
 
     if letra_is_valid(letra):
         if letra_is_correct(letra, secret_word):
-            letras_correctas += letra
+            if letra.lower() not in letras_correctas:
+                letras_correctas += letra.lower()
         else:
-            letras_incorrectas += letra
+            if letra.lower() not in letras_incorrectas:
+                letras_incorrectas += letra.lower()
+
             vidas -= 1
 
     if is_win(secret_word, letras_correctas):
-        print("Enhorabuena Has ganado")
+        print(f"\n¡¡HAS GANADO!!\nLa palabra secreta era: {secret_word}")
         fin = 0
     elif vidas == 0:
-        print("GAME OVER")
+        print(f"\nVIDAS: {vidas}\nGAME OVER\n\nSecret Word: {secret_word}")
         fin = 0
+
+    print()
